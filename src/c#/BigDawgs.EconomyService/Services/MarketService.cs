34ecb73205299bc8e019938bfd40783856095895
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2026-04-21 14:44:08 +0200

    setup + dockerfile for testing of deploy

---
+++ src/c#/BigDawgs.EconomyService/Services/MarketService.cs
@@ -0,0 +1,14 @@
+﻿namespace BigDawgs.EconomyService.Services
+{
+    public class MarketService
+    {
+        public object GetPrices()
+        {
+            return new
+            {
+                bonesOfMeat = 10,
+                dogCoins = 5
+            };
+        }
+    }
+}
