92d3e2d1f3d0677f7968db43681f4a08975df858
krixzy <mortensenkim@msn.com>
2026-05-01 11:36:19 +0200

    small changes

--- src/big_dog_rise_of_the_pack_rails/app/controllers/marketplace_controller.rb
+++ src/big_dog_rise_of_the_pack_rails/app/controllers/marketplace_controller.rb
@@ -1,7 +1,11 @@
 class MarketplaceController < ApplicationController
   def show
     @market_place =  JSON.parse(File.read(Rails.root.join("buildings.json"))).find { |hash| hash["Key"] == "market"}
-    puts "Market place data: #{@market_place.fetch("Level")}"
+    @price = FaradayService.fetch_data("/market/prices", connection_type: "MARKETPLACE_BASE_URL")
+    puts "______________________________"
+    puts "MARKETPLACE PRICE: #{@price}"
+    puts "______________________________"
+
   end
 
   def create
--- src/big_dog_rise_of_the_pack_rails/app/services/faraday_service.rb
+++ src/big_dog_rise_of_the_pack_rails/app/services/faraday_service.rb
@@ -1,17 +1,18 @@
 class FaradayService
 
-  def self.post(url, token: nil, body: nil)
-    new(url).post_request(token: token, body: body)
+  def self.post(url, connection_type: "ENGINE_BASE_URL", token: nil, body: nil)
+    new(url, connection_type).post_request(token: token, body: body)
   end
 
-  def self.fetch_data(url, token: nil)
-    new(url).fetch_data(token: token)
+  def self.fetch_data(url, connection_type: "ENGINE_BASE_URL", token: nil)
+    new(url, connection_type).fetch_data(token: token)
   end
 
-  attr_reader :url
+  attr_reader :url, :connection_type
 
-  def initialize(url)
+  def initialize(url, connection_type)
     @url = url
+    @connection_type = connection_type
   end
 
   def post_request(token: nil, body: nil)
@@ -23,7 +24,7 @@ class FaradayService
   
 
   def faraday_connection
-    @faraday_connection ||= Faraday.new(url: ENV.fetch('ENGINE_BASE_URL', 'localhost:3000')) do |faraday|
+    @faraday_connection ||= Faraday.new(url: ENV.fetch(connection_type, 'localhost:5432')) do |faraday|
       faraday.request :json
       faraday.response :json
       faraday.adapter Faraday.default_adapter
@@ -34,7 +35,7 @@ class FaradayService
     response = faraday_connection.get(url) do |request|
       request.headers["Authorization"] = "Bearer #{token}" if token
     end
-    response.body
+    response&.body
   end
 
 end
